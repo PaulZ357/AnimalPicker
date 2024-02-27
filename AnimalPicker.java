b5f5f50b333b308dc1884e0c7fa6d4d04744f7e9
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-26 21:19:26 -0500

    code

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,14 +4,18 @@ public class AnimalPicker {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Which animal would you like to see? ");
-        String word = keyboard.next();
-        if (word.equalsIgnoreCase("cat")) {
-            System.out.println("CAT");
-        } else if (word.equalsIgnoreCase("dog")) {
-            System.out.println("DOG");
-        } else {
-            System.out.println("invalid option");
-        }
+        String word;
+        do {
+            word = keyboard.nextLine();
+            if (word.equalsIgnoreCase("cat")) {
+                System.out.println("CAT");
+            } else if (word.equalsIgnoreCase("dog")) {
+                System.out.println("DOG");
+            } else {
+                // error message
+                System.out.print("Invalid animal. ");
+            }
+        } while (!word.equalsIgnoreCase("cat") && !word.equalsIgnoreCase("dog"));
         keyboard.close();
     }
 }
