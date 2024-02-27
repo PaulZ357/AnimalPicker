09ad693502b7b2a3f485cb6b3199a14f8b2915b7
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-26 20:59:13 -0500

    first commit

---
+++ AnimalPicker.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Which animal would you like to see? ");
+        String word = keyboard.next();
+        if (word.equalsIgnoreCase("cat")) {
+            System.out.println("CAT");
+        } else if (word.equalsIgnoreCase("dog")) {
+            System.out.println("DOG");
+        } 
+        keyboard.close();
+    }
+}
